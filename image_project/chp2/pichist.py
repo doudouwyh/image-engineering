7c7aa3f4759bfa910880638ec2dfd0f22d807c45
tonghstone <wenhuan193@pingan.com.cn>
2017-07-25 20:54:58 +0800

    update

--- image_project/chp2/pichist.py
+++ image_project/chp2/pichist.py
@@ -20,21 +20,129 @@ def get_counts(img):
             counts[img[i,j]] += 1
     return counts
 
+def getcdf(a):
+    return np.cumsum(a)/(np.sum(a)*1.0)
+
 def hist_enhance(image):
     width,height = image.shape
     counts = get_counts(image)
     keys = counts.keys()
-    cdf = counts.values()/(np.sum(counts.values())*1.0)
+    cdf = getcdf(counts.values())
     size =  len(keys)-1
     dict = {}
-    for i in range(size):
+    for i in keys:
         dict[i] = int(size * cdf[i] + 0.5)
     newdata = np.zeros(image.shape)
     for i in range(width):
         for j in range(height):
-            newdata[i,j] = dict[image[i,j]]
+            newdata[i,j] = dict[image[i,j]] * keys[-1]
 
     print newdata
+
+#dest: spec dict
+def hist_spec_SML(image,dest):
+    counts = get_counts(image)
+    keys = counts.keys()
+    size = len(keys)-1
+
+    destkeys = np.copy(keys)
+    destvalues = []
+    for k in destkeys:
+        if  k in dest:
+            destvalues[k] = dest[k]
+        else:
+            destvalues[k] = 0
+
+    srccdf = getcdf(counts.values())
+    destcdf = getcdf(destvalues)
+
+    srcmin = np.array([0.0]*size**2).reshape(size,size)
+    for x in range(size):
+        for y in range(size):
+            srcmin[y,x] = srccdf[x] - destcdf[y]
+
+    specmap = {}
+    for x in range(size):
+        miny = 0
+        minv = srcmin[0,x]
+        for y in range(1,size):
+            if minv > srcmin[y,x]:
+                minv = srcmin[y,x]
+                miny = y
+        specmap[y] = miny
+
+    newdata = np.zeros(image.shape)
+    width,height = image.shape
+    for i in range(width):
+        for j in range(height):
+            newdata[i,j] = specmap[image[i,j]]
+
+    return newdata
+
+def hist_spec_GML(image,dest):
+    counts = get_counts(image)
+    keys = counts.keys()
+    size = len(keys)-1
+
+    destkeys = np.copy(keys)
+    destvalues = []
+    for k in destkeys:
+        if  k in dest:
+            destvalues[k] = dest[k]
+        else:
+            destvalues[k] = 0
+
+    srccdf = getcdf(counts.values())
+    destcdf = getcdf(destvalues)
+
+    srcmin = np.array([0.0]*size**2).reshape(size,size)
+    for x in range(size):
+        for y in range(size):
+            srcmin[y,x] = srccdf[x] - destcdf[y]
+
+    specmap = {}
+    laststarty = 0
+    lastendy = 0
+    starty = 0
+    endy = 0
+    for x in range(size):
+        minv = srcmin[x,0]
+        for y in range(1,size):
+            if minv > srcmin[x,y]:
+                minv = srcmin[x,y]
+                endy = y
+
+        if starty != laststarty or endy != lastendy:
+            for i in range(starty,endy):
+                specmap[i] = x
+            laststarty = starty
+            lastendy = endy
+            starty = lastendy + 1
+
+    newdata = np.zeros(image.shape)
+    width,height = image.shape
+    for i in range(width):
+        for j in range(height):
+            newdata[i,j] = specmap[image[i,j]]
+
+    return newdata
+
+
+def hist_spec_SML_test():
+    data = get_image_data("../pic/lena.jpg")
+    Image.fromarray(data.astype(np.uint8)).show("origin")
+
+    spec = {80:0.2,160:0.6,240:0.2}
+    newdata = hist_spec_SML(data,spec)
+    Image.fromarray(newdata.astype(np.uint8)).show("last")
+
+def hist_spec_GML_test():
+    data = get_image_data("../pic/lena.jpg")
+    Image.fromarray(data.astype(np.uint8)).show("origin")
+
+    spec = {80:0.2,160:0.6,240:0.2}
+    newdata = hist_spec_GML(data,spec)
+    Image.fromarray(newdata.astype(np.uint8)).show("last")
 
 
 def hist_enhance_test():
@@ -49,4 +157,6 @@ def hist_enhance_test():
 
 if __name__ == '__main__':
     hist_enhance_test()
+    hist_spec_SML_test()
+    hist_spec_GML_test()
 
---
+++ image_project/chp3/picspace.py
@@ -0,0 +1,103 @@
+'''
+    template operation
+'''
+from PIL import Image
+import numpy as np
+
+
+def get_image_data(filename):
+    im = Image.open(filename)
+    data = im.getdata()
+    data = np.array(data,'float').reshape(im.size)
+    return data
+
+def conv(image,x,y,template):
+    w,h = template.shape
+    c = []
+    count = 0
+    for i in range(x-w/2,x+w/2+1):
+        for j in range(y-h/2,y+h/2+1):
+            if i<0 or j<0:
+                c.append(0)
+            else:
+                c.append(image[i,j])
+                count += template[i+w/2,j+h/2]
+    template.reshape(1,w*h)
+    return np.sum(np.array(c) * template),count
+
+def template_conv(image,template):
+    width,height = image.shape
+    newdata = np.zeros(image.shape)
+    for i in range(width):
+        for j in range(height):
+            newdata[i,j],count = conv(image,i,j,template)
+            if newdata[i,j] < 0:
+                newdata[i,j] = 0
+            if newdata[i,j] > 255:
+                newdata[i,j] = 255
+
+    return newdata
+
+#flag:  1-smooth, 2-sharpen
+def linear_filter(image,template,flag):
+    width,height = image.shape
+    newdata = np.zeros(image.shape)
+    for i in range(width):
+        for j in range(height):
+            newdata[i,j],count = conv(image,i,j,template)
+            if newdata[i,j] < 0:
+                newdata[i,j] = 0
+            if newdata[i,j] > 255:
+                newdata[i,j] = 255
+            if flag == 1:
+                newdata[i,j] = newdata/(count*1.0)
+
+
+    return newdata
+
+#high-frequency emphasis filtering
+# A : coefficient
+def hfe_filter(image,A):
+    #gauss smooth
+    gauss = np.array([1,4,7,4,1,4,16,26,16,4,7,26,41,26,7,4,16,26,16,4,1,4,7,4,1]).reshape(5,5)
+    smoothimg = linear_filter(image,gauss,1)
+
+    #unsharp mask
+    um = image - smoothimg
+
+    #high-frequency emphasis
+
+
+
+def template_conv_test():
+    data = get_image_data("../pic/lena.jpg")
+    Image.fromarray(data.astype(np.uint8)).show("origin")
+
+    sobel = np.array([-2,-1,0,-1,0,1,0,1,2]).reshape(3,3)
+    last = template_conv(data,sobel)
+    Image.fromarray(last.astype(np.uint8)).show("last")
+
+def linear_filter_test():
+    data = get_image_data("../pic/lena.jpg")
+    Image.fromarray(data.astype(np.uint8)).show("origin")
+
+    #weighted mean
+    sobel = np.array([1,2,1,2,4,2,1,2,1]).reshape(3,3)
+    lastmean = linear_filter(data,sobel,1)
+    Image.fromarray(lastmean.astype(np.uint8)).show("mean")
+
+    #gauss
+    gauss = np.array([1,4,7,4,1,4,16,26,16,4,7,26,41,26,7,4,16,26,16,4,1,4,7,4,1]).reshape(5,5)
+    lastgauss = linear_filter(data,gauss,1)
+    Image.fromarray(lastgauss.astype(np.uint8)).show("gauss")
+
+    #laplace
+    laplace = np.array([0,-1,0,-1,4,-1,0,-1,0]).reshape(3,3)
+    lastlap = linear_filter(data,laplace,2)
+    Image.fromarray(lastlap.astype(np.uint8)).show("laplace")
+
+
+
+if __name__ == '__main__':
+    template_conv_test()
+    linear_filter_test()
